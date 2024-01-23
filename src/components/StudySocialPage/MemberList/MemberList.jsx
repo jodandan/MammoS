79abcabe7802efb1c083543058b438516ae45f03
KoungQ <guu119@naver.com>
2024-01-23 17:23:27 +0900

    fix: 스터디 페이지 초기 랜더링 오류 수정

--- src/components/StudySocialPage/MemberList/MemberList.jsx
+++ src/components/StudySocialPage/MemberList/MemberList.jsx
@@ -2,52 +2,68 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { Box, NameAndId, College, Position, Container } from './MemberListCss.jsx';
+import {
+  Box,
+  NameAndId,
+  College,
+  Position,
+  Container,
+} from './MemberListCss.jsx';
 
 export default function MemberList({ currentIndex }) {
-    const [studyData, setStudyData] = useState(null);
-    const navigate = useNavigate();
-    const [projectIndex, setProjectIndex] = useState(0);
+  const [studyData, setStudyData] = useState(null);
+  const navigate = useNavigate();
+  const [projectIndex, setProjectIndex] = useState(0);
 
-    const handleProjectSelect = (index) => {
-        setProjectIndex(index);
+  const handleProjectSelect = (index) => {
+    setProjectIndex(index);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await axios.get(
+          'http://3.38.7.193:8080/api/v1/study',
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setStudyData(response.data.responseData);
+        console.log('소셜 페이지에서의 인덱스:', currentIndex);
+      } catch (error) {
+        console.error('Error fetching study information:', error);
+      }
     };
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const token = localStorage.getItem('token');
-                const response = await axios.get('http://3.38.7.193:8080/api/v1/study', {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                });
-                setStudyData(response.data.responseData);
-                console.log('소셜 페이지에서의 인덱스:', currentIndex);
-            } catch (error) {
-                console.error('Error fetching study information:', error);
-            }
-        };
-
-        fetchData();
-    }, [currentIndex]);
-    return (
-        <Container>
-            {studyData && (
-                <>
-                    {studyData[currentIndex].social.members.map((member, memberIndex) => (
-                        <Box key={memberIndex}>
-                            <NameAndId>{member.name} / {member.id}</NameAndId>
-                            <College>{member.universityName} {member.majorName}</College>
-                            <Position>{member.status}</Position>
-                        </Box>
-                    ))}
-                </>
+    fetchData();
+  }, [currentIndex]);
+  return (
+    <Container>
+      {studyData && (
+        <>
+          {studyData[currentIndex] &&
+            studyData[currentIndex].social.members.map(
+              (member, memberIndex) => (
+                <Box key={memberIndex}>
+                  <NameAndId>
+                    {member.name} / {member.id}
+                  </NameAndId>
+                  <College>
+                    {member.universityName} {member.majorName}
+                  </College>
+                  <Position>{member.status}</Position>
+                </Box>
+              )
             )}
-        </Container>
-    )
+        </>
+      )}
+    </Container>
+  );
 }
 
 MemberList.propTypes = {
-    currentIndex: PropTypes.number.isRequired,
+  currentIndex: PropTypes.number.isRequired,
 };
