011ddd225beb1c1482dd2ff772bab896abe2d82d
KoungQ <guu119@naver.com>
2024-01-23 17:09:03 +0900

    fix: 스터디 페이지 초기 랜더링 오류 수정

--- src/components/StudyPage/StudyHome.jsx
+++ src/components/StudyPage/StudyHome.jsx
@@ -4,17 +4,17 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import NavigationBar from '../PageFrame/pageFrameItems/NavigationBar.jsx';
 import {
-    FrameContainer,
-    ContainerBox,
-    StudyTitle,
-    SubText,
-    TextBox,
-    SecondLine,
-    SideMenuBar,
-    Img,
-    Container,
-    CheckContainer,
-    Pagingbox,
+  FrameContainer,
+  ContainerBox,
+  StudyTitle,
+  SubText,
+  TextBox,
+  SecondLine,
+  SideMenuBar,
+  Img,
+  Container,
+  CheckContainer,
+  Pagingbox,
 } from './StudyPageCss.jsx';
 import StudyContainer from './StudyPageItems/StudyBox/StudyContainer.jsx';
 import MemberContainer from './StudyPageItems/MemberBox/MemberContainer.jsx';
@@ -24,89 +24,105 @@ import Calender from '../assets/Calender.png';
 import User from '../assets/User.png';
 import List from '../assets/List.png';
 import { ReactComponent as LeftArrow } from '../assets/LeftArrow.svg';
-import { ReactComponent as RightArrow } from '../assets/RightArrow.svg'
-
+import { ReactComponent as RightArrow } from '../assets/RightArrow.svg';
 
 export default function StudyHome({ onIndexChange }) {
-    const navigate = useNavigate();
-    const [studyData, setStudyData] = useState(null);
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const navigate = useNavigate();
+  const [studyData, setStudyData] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    const handleLeftArrowClick = () => {
-        if (currentIndex > 0) {
-            setCurrentIndex(currentIndex - 1);
-            onIndexChange(currentIndex - 1); // Pass updated index
-        }
+  const handleLeftArrowClick = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+      onIndexChange(currentIndex - 1); // Pass updated index
+    }
+  };
+
+  const handleRightArrowClick = () => {
+    if (currentIndex < studyData.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+      onIndexChange(currentIndex + 1); // Pass updated index
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        console.log(token);
+        const response = await axios.get(
+          'http://3.38.7.193:8080/api/v1/study',
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setStudyData(response.data.responseData);
+      } catch (error) {
+        console.error('Error fetching study information:', error);
+      }
     };
-
-    const handleRightArrowClick = () => {
-        if (currentIndex < studyData.length - 1) {
-            setCurrentIndex(currentIndex + 1);
-            onIndexChange(currentIndex + 1); // Pass updated index
-        }
-    };
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const token = localStorage.getItem('token');
-                console.log(token);
-                const response = await axios.get('http://3.38.7.193:8080/api/v1/study', {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                });
-                setStudyData(response.data.responseData);
-            } catch (error) {
-                console.error('Error fetching study information:', error);
-            }
-        };
-        // 메모이즈된 함수를 사용
-        onIndexChange(currentIndex);
-        fetchData();
-    }, [currentIndex, onIndexChange]);
-    return (
-        <>
-            <NavigationBar />
-            <Pagingbox>
-                <LeftArrow onClick={handleLeftArrowClick} />
-                <RightArrow onClick={handleRightArrowClick} />
-            </Pagingbox>
-            <FrameContainer>
-                <SideMenuBar>
-                    <CheckContainer>
-                        <Img onClick={() => navigate('/studyHome')} src={Clickhome} alt='홈' />
-                    </CheckContainer>
-                    <Container>
-                        <Img onClick={() => navigate('/studyCalender')} src={Calender} alt='캘린더' />
-                    </Container>
-                    <Container>
-                        <Img onClick={() => navigate('/studySocial')} src={User} alt='유저' />
-                    </Container>
-                    <Container>
-                        <Img src={List} alt='리스트' />
-                    </Container>
-                </SideMenuBar>
-                <ContainerBox>
-                    {studyData && (
-                        <TextBox>
-                            <StudyTitle>{studyData[currentIndex].home.title || '없음'}</StudyTitle>
-                            <SubText>{studyData[currentIndex].summary || '없음'}</SubText>
-                        </TextBox>
-                    )}
-                    <StudyContainer currentIndex={currentIndex} />
-                    {studyData && (
-                        <SecondLine>
-                            <MemberContainer currentIndex={currentIndex} />
-                            <NoticeContainer currentIndex={currentIndex} />
-                        </SecondLine>
-                    )}
-                </ContainerBox>
-            </FrameContainer >
-        </>
-    )
+    // 메모이즈된 함수를 사용
+    onIndexChange(currentIndex);
+    fetchData();
+  }, [currentIndex, onIndexChange]);
+  return (
+    <>
+      <NavigationBar />
+      <Pagingbox>
+        <LeftArrow onClick={handleLeftArrowClick} />
+        <RightArrow onClick={handleRightArrowClick} />
+      </Pagingbox>
+      <FrameContainer>
+        <SideMenuBar>
+          <CheckContainer>
+            <Img
+              onClick={() => navigate('/studyHome')}
+              src={Clickhome}
+              alt="홈"
+            />
+          </CheckContainer>
+          <Container>
+            <Img
+              onClick={() => navigate('/studyCalender')}
+              src={Calender}
+              alt="캘린더"
+            />
+          </Container>
+          <Container>
+            <Img
+              onClick={() => navigate('/studySocial')}
+              src={User}
+              alt="유저"
+            />
+          </Container>
+          <Container>
+            <Img src={List} alt="리스트" />
+          </Container>
+        </SideMenuBar>
+        <ContainerBox>
+          {studyData && (
+            <TextBox>
+              <StudyTitle>
+                {studyData[currentIndex].home.title || '없음'}
+              </StudyTitle>
+              <SubText>{studyData[currentIndex].summary || '없음'}</SubText>
+            </TextBox>
+          )}
+          <StudyContainer currentIndex={currentIndex} />
+          {studyData && (
+            <SecondLine>
+              <MemberContainer currentIndex={currentIndex} />
+              <NoticeContainer currentIndex={currentIndex} />
+            </SecondLine>
+          )}
+        </ContainerBox>
+      </FrameContainer>
+    </>
+  );
 }
 
 StudyHome.propTypes = {
-    onIndexChange: PropTypes.func.isRequired,
+  onIndexChange: PropTypes.func.isRequired,
 };
--- src/components/StudyPage/StudyPageItems/StudyBox/StudyContainer.jsx
+++ src/components/StudyPage/StudyPageItems/StudyBox/StudyContainer.jsx
@@ -20,9 +20,7 @@ import {
   ListTitle,
   PeopleList,
   CheckBoxLabel,
-
 } from './StudyContainerCss';
-
 
 export default function StudyContainer({ currentIndex }) {
   const [studyData, setStudyData] = useState(null);
@@ -31,11 +29,14 @@ export default function StudyContainer({ currentIndex }) {
     const fetchData = async () => {
       try {
         const token = localStorage.getItem('token');
-        const response = await axios.get('http://3.38.7.193:8080/api/v1/study', {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+        const response = await axios.get(
+          'http://3.38.7.193:8080/api/v1/study',
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
 
         setStudyData(response.data.responseData);
       } catch (error) {
@@ -49,24 +50,49 @@ export default function StudyContainer({ currentIndex }) {
     <TotalBox>
       <BoxTitle>최근스터디</BoxTitle>
       <Container>
-      {studyData && studyData[currentIndex]?.home && (
+        {studyData && studyData[currentIndex]?.home && (
           <FirstLine>
-            <LeftSide>{studyData[currentIndex]?.home?.projectTabResponseDto?.projectTitle || '없음'}</LeftSide>
-            <DataBox>{studyData[currentIndex]?.home?.projectTabResponseDto?.startDate} ~ {studyData[currentIndex]?.home?.projectTabResponseDto?.endDate}</DataBox>
-            <LocationBox>{studyData[currentIndex]?.home?.projectTabResponseDto?.place || '없음'}</LocationBox>
+            <LeftSide>
+              {(studyData[currentIndex].home.projectTabResponseDto &&
+                studyData[currentIndex]?.home?.projectTabResponseDto
+                  ?.projectTitle) ||
+                '없음'}
+            </LeftSide>
+            <DataBox>
+              {studyData[currentIndex]?.home?.projectTabResponseDto?.startDate}{' '}
+              ~ {studyData[currentIndex]?.home?.projectTabResponseDto?.endDate}
+            </DataBox>
+            <LocationBox>
+              {(studyData[currentIndex].home.projectTabResponseDto &&
+                studyData[currentIndex]?.home?.projectTabResponseDto?.place) ||
+                '없음'}
+            </LocationBox>
           </FirstLine>
         )}
         <SecondLine>
           <NoticeBox>
-            <div style={{ display: 'flex', flexDirection: 'columns', padding: '0.5rem 0.5rem 0.1rem 0.5rem' }}>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'columns',
+                padding: '0.5rem 0.5rem 0.1rem 0.5rem',
+              }}
+            >
               <Title>Study Summary</Title>
               <EditBox>
-                <img src={Edit} alt='수정' style={{ width: '12px', height: '12px' }} />
+                <img
+                  src={Edit}
+                  alt="수정"
+                  style={{ width: '12px', height: '12px' }}
+                />
               </EditBox>
             </div>
             {studyData && studyData[currentIndex]?.home && (
               <InputBox>
-                {studyData[currentIndex].home.projectTabResponseDto.studySummary || '없음'}
+                {(studyData[currentIndex].home.projectTabResponseDto &&
+                  studyData[currentIndex].home.projectTabResponseDto
+                    .studySummary) ||
+                  '없음'}
               </InputBox>
             )}
           </NoticeBox>
@@ -74,23 +100,28 @@ export default function StudyContainer({ currentIndex }) {
             <ListTitle>참여 인원</ListTitle>
             {studyData && studyData[currentIndex]?.home && (
               <PeopleList>
-                {studyData[currentIndex].home.projectTabResponseDto.projectInMembers
-                  ? studyData[currentIndex].home.projectTabResponseDto.projectInMembers.map((member) => (
-                    <CheckBoxLabel key={member.id}>
-                      <input type="checkbox" />
-                      <TextBox>{member.name || '없음'} /</TextBox>
-                      <TextBox2>&nbsp;{member.id || '없음'}</TextBox2>
-                    </CheckBoxLabel>
-                  ))
-                  : '없음'
-                }
+                {studyData[currentIndex].home.projectTabResponseDto &&
+                studyData[currentIndex].home.projectTabResponseDto
+                  .projectInMembers
+                  ? studyData[
+                      currentIndex
+                    ].home.projectTabResponseDto.projectInMembers.map(
+                      (member) => (
+                        <CheckBoxLabel key={member.id}>
+                          <input type="checkbox" />
+                          <TextBox>{member.name || '없음'} /</TextBox>
+                          <TextBox2>&nbsp;{member.id || '없음'}</TextBox2>
+                        </CheckBoxLabel>
+                      )
+                    )
+                  : '없음'}
               </PeopleList>
             )}
           </PeopleListBox>
         </SecondLine>
       </Container>
     </TotalBox>
-  )
+  );
 }
 
 export const TextBox = styled.div`
@@ -102,9 +133,7 @@ export const TextBox = styled.div`
   letter-spacing: -0.32px;
 `;
 
-export const TextBox2 = styled(TextBox)`
-`;
-
+export const TextBox2 = styled(TextBox)``;
 
 StudyContainer.propTypes = {
   currentIndex: PropTypes.number.isRequired,
