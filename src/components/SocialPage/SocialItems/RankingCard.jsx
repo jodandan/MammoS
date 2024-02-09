3f0e0c6af76701653b76135cfd223d6c987d8eac
KoungQ <guu119@naver.com>
2024-02-09 22:08:09 +0900

    fix: 랭킹 등수 표시

--- src/components/SocialPage/SocialItems/RankingCard.jsx
+++ src/components/SocialPage/SocialItems/RankingCard.jsx
@@ -2,41 +2,41 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-
 const RankingCardBox = styled.div`
   align-items: center;
-  margin-right: 2vw; 
-  margin-left: 2vw; 
+  margin-right: 2vw;
+  margin-left: 2vw;
   margin-top: 3vw;
 `;
 
-
 const RankCard = styled.div`
-  background-color: #d9d9d9; 
-  border-radius: 35px; 
+  background-color: #d9d9d9;
+  border-radius: 35px;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: 7vw; 
+  width: 7vw;
   height: 7vw;
-  padding: 1rem; 
-  margin-top: ${({ isTopRank }) => isTopRank ? '5vw' : '7vw'}; // 1등일 때 크기를 1.5배로 증가
+  padding: 1rem;
+  margin-top: ${({ isTopRank }) =>
+    isTopRank ? '5vw' : '7vw'}; // 1등일 때 크기를 1.5배로 증가
   position: relative;
-  transform: ${({ isTopRank }) => isTopRank ? 'scale(1.5)' : 'none'}; // 1등일 때 크기를 1.5배로 증가
+  transform: ${({ isTopRank }) =>
+    isTopRank ? 'scale(1.5)' : 'none'}; // 1등일 때 크기를 1.5배로 증가
 `;
 
 const RankBadge = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  top: -10px; 
+  top: -10px;
   left: 50%;
-  background-color: rgba(199, 224, 135, 0.5); 
-  color: black; 
-  box-shadow: 0px 4px 2px rgba(0, 0, 0, 0.5); 
-  border-radius: 50%; 
-  width: 2.5vw; 
-  height: 2.5vw; 
+  background-color: rgba(199, 224, 135, 0.5);
+  color: black;
+  box-shadow: 0px 4px 2px rgba(0, 0, 0, 0.5);
+  border-radius: 50%;
+  width: 2.5vw;
+  height: 2.5vw;
   font-weight: bold;
   font-size: 1.3vw;
   position: absolute;
@@ -49,68 +49,62 @@ const RankingCardFont = styled.p`
   font-size: 13px;
   padding: 5px;
   margin: 0px;
-  
 `;
 
-const FontBox= styled.div`
+const FontBox = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: ${({ isTopRank }) => isTopRank ? '5vw' : '2vw'}; // 1등일 때 크기를 1.5배로 증가
-`
+  margin-top: ${({ isTopRank }) =>
+    isTopRank ? '5vw' : '2vw'}; // 1등일 때 크기를 1.5배로 증가
+`;
 
 const FontRow = styled.div`
   display: flex;
   justify-content: center;
 `;
 
+const RankingCard = ({ rank, pfp, id, name, weeklyStudyTime }) => {
+  const isTopRank = rank === 1; // 1등인지 여부 확인
 
-const RankingCard = ({
-    rank,
-    pfp,
-    id,
-    name,
-    weeklyStudyTime
-}) => {
-    const isTopRank = rank === 1; // 1등인지 여부 확인
+  const formatTime = (minutes) => {
+    const totalTimeHour = String(Math.floor(minutes / 60)).padStart(2, '0');
+    const totalTimeMin = String(Math.floor(minutes % 60)).padStart(2, '0');
+    return `${totalTimeHour}:${totalTimeMin}`;
+  };
 
-    const formatTime = (minutes) => {
-        const totalTimeHour = String(Math.floor(minutes / 60)).padStart(2, '0');
-        const totalTimeMin = String(Math.floor(minutes % 60)).padStart(2, '0');
-        return `${totalTimeHour}:${totalTimeMin}`;
-    };
+  // 주간 공부 시간이 유효한 경우에만 포매팅
+  const formattedWeeklyStudyTime = Number.isFinite(weeklyStudyTime)
+    ? formatTime(weeklyStudyTime)
+    : '';
 
-    // 주간 공부 시간이 유효한 경우에만 포매팅
-    const formattedWeeklyStudyTime = Number.isFinite(weeklyStudyTime)
-        ? formatTime(weeklyStudyTime)
-        : '';
-
-
-    return (
-        <RankingCardBox>
-            <RankCard isTopRank={isTopRank}>
-                <RankBadge>{rank}</RankBadge>
-            </RankCard>
-            <FontBox isTopRank={isTopRank}>
-                <FontRow>
-                    <RankingCardFont>{name}</RankingCardFont>
-                    <RankingCardFont>{id}</RankingCardFont>
-                </FontRow>
-                {Number.isFinite(weeklyStudyTime) && (
-                    <FontRow>
-                        <RankingCardFont>WeekTime | {formattedWeeklyStudyTime}</RankingCardFont>
-                    </FontRow>
-                )}
-            </FontBox>
-        </RankingCardBox>
-    );
+  return (
+    <RankingCardBox>
+      <RankCard isTopRank={isTopRank}>
+        <RankBadge>{rank}</RankBadge>
+      </RankCard>
+      <FontBox isTopRank={isTopRank}>
+        <FontRow>
+          <RankingCardFont>{name}</RankingCardFont>
+          <RankingCardFont>{id}</RankingCardFont>
+        </FontRow>
+        {Number.isFinite(weeklyStudyTime) && (
+          <FontRow>
+            <RankingCardFont>
+              WeekTime | {formattedWeeklyStudyTime}
+            </RankingCardFont>
+          </FontRow>
+        )}
+      </FontBox>
+    </RankingCardBox>
+  );
 };
 
 RankingCard.propTypes = {
-    rank: PropTypes.number.isRequired, // rank prop 타입 지정
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    weeklyStudyTime: PropTypes.number.isRequired,
-    pfp: PropTypes.string.isRequired
+  rank: PropTypes.number.isRequired, // rank prop 타입 지정
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  weeklyStudyTime: PropTypes.number.isRequired,
+  pfp: PropTypes.string.isRequired,
 };
 
 export default RankingCard;
--- src/components/SocialPage/SocialItems/RankingSection.jsx
+++ src/components/SocialPage/SocialItems/RankingSection.jsx
@@ -7,58 +7,63 @@ const RankingSectionBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 30vw
+  width: 30vw;
 `;
 
-const RankingSection = ({
-  ranking
-}) => {
+const RankingSection = ({ ranking }) => {
+  // 랭킹을 주간 공부 시간에 따라 정렬하고 원래 순위를 기억
+  const sortedRanking = [...ranking]
+    .sort((a, b) => b.weeklyStudyTime - a.weeklyStudyTime)
+    .map((item, index) => ({ ...item, originalRank: index + 1 }));
 
-    // 랭킹을 주간 공부 시간에 따라 정렬하고 원래 순위를 기억
-    const sortedRanking = [...ranking]
-        .sort((a, b) => b.weeklyStudyTime - a.weeklyStudyTime)
-        .map((item, index) => ({ ...item, originalRank: index + 1 }));
+  let reorderedRanking = [];
+  if (sortedRanking.length >= 3) {
+    // 1등을 가운데, 2등을 왼쪽, 3등을 오른쪽에 배치
+    reorderedRanking = [sortedRanking[1], sortedRanking[0], sortedRanking[2]];
+  } else if (sortedRanking.length === 2) {
+    // 1등을 왼쪽, 2등을 왼쪽에 배치
+    reorderedRanking = [sortedRanking[1], sortedRanking[0], {}];
+  } else if (sortedRanking.length === 1) {
+    // 1등만 표시
+    reorderedRanking = [{}, sortedRanking[0], {}];
+  } else {
+    // 랭킹 정보가 없을 경우
+    reorderedRanking = [{}, {}, {}];
+  }
 
+  function makeRanking() {
+    const result = [];
+    let cnt = 0;
+    const ranking = [2, 1, 3];
 
-    let reorderedRanking = [];
-    if (sortedRanking.length >= 3) {
-        // 1등을 가운데, 2등을 왼쪽, 3등을 오른쪽에 배치
-        reorderedRanking = [sortedRanking[1], sortedRanking[0], sortedRanking[2]];
-    } else if (sortedRanking.length === 2) {
-        // 1등을 왼쪽, 2등을 왼쪽에 배치
-        reorderedRanking = [sortedRanking[1], sortedRanking[0], {}];
-    } else if (sortedRanking.length === 1) {
-        // 1등만 표시
-        reorderedRanking = [{}, sortedRanking[0], {}];
-    } else {
-        // 랭킹 정보가 없을 경우
-        reorderedRanking = [{}, {}, {}];
-    }
+    reorderedRanking.map((rank) => {
+      result.push(
+        <RankingCard
+          key={rank.id}
+          rank={ranking[cnt]} // 랭킹 번호 전달
+          id={rank.id}
+          name={rank.name}
+          pfp={rank.pfp}
+          weeklyStudyTime={rank.weeklyStudyTime}
+        />
+      );
+      cnt++;
+    });
 
-    return (
-        <RankingSectionBox>
-            {reorderedRanking.map((rank) => (
-                <RankingCard
-                    key={rank.id}
-                    rank={rank.originalRank} // 랭킹 번호 전달
-                    id={rank.id}
-                    name={rank.name}
-                    pfp={rank.pfp}
-                    weeklyStudyTime={rank.weeklyStudyTime}
-                />
-            ))}
-        </RankingSectionBox>
-    );
+    return result;
+  }
+
+  return <RankingSectionBox>{makeRanking()}</RankingSectionBox>;
 };
 
 RankingSection.propTypes = {
-    ranking: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            pfp: PropTypes.string.isRequired,
-            weeklyStudyTime: PropTypes.number.isRequired,
-        })
-    ).isRequired,
+  ranking: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      pfp: PropTypes.string.isRequired,
+      weeklyStudyTime: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 };
 export default RankingSection;
